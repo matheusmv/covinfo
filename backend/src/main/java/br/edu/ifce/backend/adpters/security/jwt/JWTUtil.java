638188fb0bf9fde9e98044e92b31273fc5644d74
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-06-09 00:06:32 -0300

    implementation of authorization filter with jwt token

--- backend/src/main/java/br/edu/ifce/backend/adpters/security/jwt/JWTUtil.java
+++ backend/src/main/java/br/edu/ifce/backend/adpters/security/jwt/JWTUtil.java
@@ -1,5 +1,6 @@
 package br.edu.ifce.backend.adpters.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,5 +23,44 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                 .compact();
+    }
+
+    public boolean tokenIsValid(String token) {
+        var claims = getClaimsToken(token);
+
+        if (claims != null) {
+            var username = claims.getSubject();
+            var expirationDate = claims.getExpiration();
+            var currentDate = new Date(System.currentTimeMillis());
+
+            System.out.println(username);
+            System.out.println(expirationDate);
+            System.out.println(currentDate);
+
+            return username != null && expirationDate != null && currentDate.before(expirationDate);
+        }
+
+        return false;
+    }
+
+    public String getUsername(String token) {
+        var claims = getClaimsToken(token);
+
+        if (claims != null) {
+            return claims.getSubject();
+        }
+
+        return null;
+    }
+
+    private Claims getClaimsToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(secret.getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
