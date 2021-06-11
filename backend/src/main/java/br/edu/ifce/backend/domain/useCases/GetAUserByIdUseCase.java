1e654018df7fcd9985716af8a6af3323ea72a3c3
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-06-10 23:55:21 -0300

    admins can remove user by id

--- backend/src/main/java/br/edu/ifce/backend/domain/useCases/GetAUserByIdUseCase.java
+++ backend/src/main/java/br/edu/ifce/backend/domain/useCases/GetAUserByIdUseCase.java
@@ -1,8 +1,6 @@
 package br.edu.ifce.backend.domain.useCases;
 
 import br.edu.ifce.backend.domain.entities.User;
-import br.edu.ifce.backend.domain.entities.enums.UserRole;
-import br.edu.ifce.backend.domain.exceptions.AuthorizationException;
 import br.edu.ifce.backend.domain.ports.driven.UserAuthenticationService;
 import br.edu.ifce.backend.domain.ports.driven.UserRepository;
 import br.edu.ifce.backend.domain.ports.driver.GetAUserById;
@@ -18,16 +16,6 @@ public class GetAUserByIdUseCase implements GetAUserById {
 
     @Override
     public User execute(Long id) {
-        var authUser = userAuthenticationService.getAuthenticatedUser();
-
-        if (authUser == null) {
-            throw new AuthorizationException("Access denied.");
-        }
-
-        if (!authUser.hasRole(UserRole.ADMIN) && !id.equals(authUser.getId())) {
-            throw new AuthorizationException("Access denied.");
-        }
-
         return userRepository.findById(id);
     }
 }
