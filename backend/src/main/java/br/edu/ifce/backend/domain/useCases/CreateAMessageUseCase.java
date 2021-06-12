8e314cd88f647bd6c96b129fdeb0489f7c25505d
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-06-12 01:44:33 -0300

    adding validation to message data

--- backend/src/main/java/br/edu/ifce/backend/domain/useCases/CreateAMessageUseCase.java
+++ backend/src/main/java/br/edu/ifce/backend/domain/useCases/CreateAMessageUseCase.java
@@ -2,10 +2,13 @@ package br.edu.ifce.backend.domain.useCases;
 
 import br.edu.ifce.backend.domain.entities.Message;
 import br.edu.ifce.backend.domain.exceptions.AuthorizationException;
+import br.edu.ifce.backend.domain.exceptions.ValidationException;
 import br.edu.ifce.backend.domain.ports.driven.MessageRepository;
 import br.edu.ifce.backend.domain.ports.driven.UserAuthenticationService;
 import br.edu.ifce.backend.domain.ports.driven.UserRepository;
 import br.edu.ifce.backend.domain.ports.driver.CreateAMessage;
+import br.edu.ifce.backend.domain.useCases.utils.MessageValidation;
+import br.edu.ifce.backend.domain.useCases.utils.MessageValidationResult;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,11 +30,24 @@ public class CreateAMessageUseCase implements CreateAMessage {
             throw new AuthorizationException("Access denied.");
         }
 
+        validateMessageData(message);
+
         var user = userRepository.findById(authUser.getId());
 
         message.setUser(user);
         message.setCreatedAt(LocalDateTime.now());
 
         messageRepository.create(message);
+    }
+
+    private void validateMessageData(Message message) {
+        MessageValidation validation = MessageValidation.titleIsValid()
+                .and(MessageValidation.contentIsValid());
+
+        MessageValidationResult result = validation.apply(message);
+
+        if (result != MessageValidationResult.SUCCESS) {
+            throw new ValidationException(result.getResult());
+        }
     }
 }
