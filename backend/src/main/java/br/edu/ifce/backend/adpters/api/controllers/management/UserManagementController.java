958ca45280fc88c70aa8b3e31fba7f8dc443cebd
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-06-15 17:25:08 -0300

    endpoint for administrators - list all accounts registered on the system

--- backend/src/main/java/br/edu/ifce/backend/adpters/api/controllers/management/UserManagementController.java
+++ backend/src/main/java/br/edu/ifce/backend/adpters/api/controllers/management/UserManagementController.java
@@ -1,9 +1,12 @@
 package br.edu.ifce.backend.adpters.api.controllers.management;
 
+import br.edu.ifce.backend.adpters.dto.userdtos.SimpleUserDTO;
 import br.edu.ifce.backend.domain.entities.User;
 import br.edu.ifce.backend.domain.ports.driver.DeleteAUserById;
 import br.edu.ifce.backend.domain.ports.driver.GetAUserById;
+import br.edu.ifce.backend.domain.ports.driver.ListAllAccountsRegisteredInTheSystem;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,7 @@ public class UserManagementController {
 
     private final GetAUserById getAUserById;
     private final DeleteAUserById deleteAUserById;
+    private final ListAllAccountsRegisteredInTheSystem listAllAccountsRegisteredInTheSystem;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getAUserById(@PathVariable Long id) {
@@ -25,5 +29,17 @@ public class UserManagementController {
     public ResponseEntity<Void> deleteAUserById(@PathVariable Long id) {
         deleteAUserById.execute(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<SimpleUserDTO>> listAllAccountsRegisteredInTheSystem(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "createdAt") String orderBy) {
+
+        var pageOfUsers = listAllAccountsRegisteredInTheSystem.execute(page, linesPerPage, direction, orderBy).map(SimpleUserDTO::new);
+
+        return ResponseEntity.ok().body(pageOfUsers);
     }
 }
---
+++ backend/src/main/java/br/edu/ifce/backend/domain/useCases/ListAllAccountsRegisteredInTheSystemUseCase.java
@@ -0,0 +1,36 @@
+package br.edu.ifce.backend.domain.useCases;
+
+import br.edu.ifce.backend.domain.entities.User;
+import br.edu.ifce.backend.domain.entities.enums.UserRole;
+import br.edu.ifce.backend.domain.exceptions.AuthorizationException;
+import br.edu.ifce.backend.domain.ports.driven.UserAuthenticationService;
+import br.edu.ifce.backend.domain.ports.driven.UserRepository;
+import br.edu.ifce.backend.domain.ports.driver.ListAllAccountsRegisteredInTheSystem;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@AllArgsConstructor
+public class ListAllAccountsRegisteredInTheSystemUseCase implements ListAllAccountsRegisteredInTheSystem {
+
+    private final UserAuthenticationService userAuthenticationService;
+    private final UserRepository userRepository;
+
+    @Override
+    public Page<User> execute(Integer page, Integer linesPerPage, String direction, String orderBy) {
+        var authUser = userAuthenticationService.getAuthenticatedUser();
+
+        if (Objects.isNull(authUser) || !authUser.hasRole(UserRole.ADMIN)) {
+            throw new AuthorizationException("Access denied.");
+        }
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return userRepository.listAll(pageRequest);
+    }
+}
