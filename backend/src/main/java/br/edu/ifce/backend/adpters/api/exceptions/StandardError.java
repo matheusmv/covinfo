27dcc9539f4b8efb55c0200be2179828b5926c03
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-06-25 18:24:01 -0300

    refactoring exception handlers and applying builder pattern

--- backend/src/main/java/br/edu/ifce/backend/adpters/api/exceptions/StandardError.java
+++ backend/src/main/java/br/edu/ifce/backend/adpters/api/exceptions/StandardError.java
@@ -1,18 +1,16 @@
 package br.edu.ifce.backend.adpters.api.exceptions;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
-@Setter
-@AllArgsConstructor
+@Builder
 public class StandardError {
-    private Instant timestamp;
-    private Integer status;
-    private String error;
-    private String message;
-    private String path;
+    private final Instant timestamp;
+    private final Integer status;
+    private final String error;
+    private final String message;
+    private final String path;
 }
