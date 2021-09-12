330cb39c2527d3e2b8d8b268dea1ca1492ed3947
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-12 14:09:09 -0300

    adding swagger documentation to the UserManagementController endpoint

--- backend/src/main/java/br/edu/ifce/backend/adpters/api/controllers/UserManagementController.java
+++ backend/src/main/java/br/edu/ifce/backend/adpters/api/controllers/UserManagementController.java
@@ -1,19 +1,26 @@
 package br.edu.ifce.backend.adpters.api.controllers;
 
+import br.edu.ifce.backend.adpters.api.docs.UserManagementControllerDocs;
 import br.edu.ifce.backend.adpters.dto.userdtos.SimpleUserDTO;
 import br.edu.ifce.backend.domain.entities.User;
 import br.edu.ifce.backend.domain.ports.driver.DeleteAUserById;
 import br.edu.ifce.backend.domain.ports.driver.GetAUserById;
 import br.edu.ifce.backend.domain.ports.driver.ListAllAccountsRegisteredInTheSystem;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/management/api/v1/users")
-@AllArgsConstructor
-public class UserManagementController {
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class UserManagementController implements UserManagementControllerDocs {
 
     private final GetAUserById getAUserById;
     private final DeleteAUserById deleteAUserById;
@@ -22,12 +29,14 @@ public class UserManagementController {
     @GetMapping("/{id}")
     public ResponseEntity<User> getAUserById(@PathVariable Long id) {
         var user = getAUserById.execute(id);
+
         return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAUserById(@PathVariable Long id) {
         deleteAUserById.execute(id);
+
         return ResponseEntity.noContent().build();
     }
 
