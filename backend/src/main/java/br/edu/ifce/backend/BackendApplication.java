2a9011d48fd55e7ac2a242c0c67ede360786ae20
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-12 12:55:54 -0300

    adjusting swagger settings

--- backend/src/main/java/br/edu/ifce/backend/BackendApplication.java
+++ backend/src/main/java/br/edu/ifce/backend/BackendApplication.java
@@ -7,7 +7,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @SpringBootApplication
 public class BackendApplication {
+
+    // TODO: corrigir documentação swagger
+    // TODO: corrigir banco de dados de teste -> criar import sql
+    // TODO: mover value objects para dto
+    // TODO: organizar use cases
+    // TODO: criar testes
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
+
 }
