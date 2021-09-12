05dc0723eb16c939ecb0439714dcfc92ac62a3d2
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-12 19:28:26 -0300

    adjusting test database settings

--- backend/src/main/java/br/edu/ifce/backend/adpters/env/config/DevConfig.java
+++ backend/src/main/java/br/edu/ifce/backend/adpters/env/config/DevConfig.java
@@ -2,7 +2,6 @@ package br.edu.ifce.backend.adpters.env.config;
 
 import br.edu.ifce.backend.adpters.email.SmtpEmailService;
 import br.edu.ifce.backend.adpters.env.utils.CreateAdminAccount;
-import br.edu.ifce.backend.adpters.env.utils.TestService;
 import br.edu.ifce.backend.domain.ports.driven.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,9 +12,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("dev")
 public class DevConfig {
-
-    @Autowired
-    private TestService testService;
 
     @Value("${spring.jpa.hibernate.ddl-auto}")
     private String ddlAutoValue;
@@ -33,12 +29,6 @@ public class DevConfig {
     public boolean instantiateDataBase() {
 
         createAdminAccount.createAdminAccount(email, password);
-
-        if (!"create".equals(ddlAutoValue)) {
-            return false;
-        }
-
-        testService.InstantiateTestDataBase();
 
         return true;
     }
