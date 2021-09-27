2f642c15df5228e580fa1869375e7bd76e61d9e3
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-27 12:23:38 -0300

    improving implementation of JWTUtil

--- backend/security/src/main/java/br/edu/ifce/security/jwt/JWTUtil.java
+++ backend/security/src/main/java/br/edu/ifce/security/jwt/JWTUtil.java
@@ -1,23 +1,30 @@
 package br.edu.ifce.security.jwt;
 
 import br.edu.ifce.security.user.UserSecurityService;
+import br.edu.ifce.usecase.exceptions.ValidationException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Payload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class JWTUtil {
+
+    private final Logger logger = LoggerFactory.getLogger(JWTUtil.class);
 
     @Value("${jwt.secret}")
     private String secret;
@@ -47,52 +54,53 @@ public class JWTUtil {
                 .sign(getSignatureAlgorithm());
     }
 
-    // TODO: handle JWTDecodeException exception
-    public boolean tokenIsValid(String authenticationToken) {
-        if (Objects.nonNull(authenticationToken)) {
-            var decodedJWT = getDecodedJWT(authenticationToken);
-
-            var username = decodedJWT.getSubject();
-            var roles = decodedJWT.getClaim("roles");
-            var expirationDate = decodedJWT.getExpiresAt();
-            var currentDate = new Date(System.currentTimeMillis());
-
-            return Objects.nonNull(username) &&
-                    Objects.nonNull(roles) &&
-                    Objects.nonNull(expirationDate) &&
-                    currentDate.before(expirationDate);
-        }
-
-        return false;
-    }
-
-    private DecodedJWT getDecodedJWT(String authenticationToken) {
-        return getJWTVerifier().verify(authenticationToken);
-    }
-
     private JWTVerifier getJWTVerifier() {
         return JWT.require(getSignatureAlgorithm()).build();
     }
 
-    public String getUsername(String authenticationToken) {
-        var decodedJWT = getDecodedJWT(authenticationToken);
-
-        if (Objects.nonNull(decodedJWT)) {
-            return decodedJWT.getSubject();
+    private DecodedJWT getDecodedJWT(String authenticationToken) {
+        try {
+            return getJWTVerifier().verify(authenticationToken);
+        } catch (JWTDecodeException e) {
+            logger.error(e.getMessage());
+            return null;
         }
+    }
 
-        return null;
+    public boolean tokenIsValid(String authenticationToken) {
+        return Optional.ofNullable(getDecodedJWT(authenticationToken))
+                .filter(usernameIsPresent)
+                .filter(rolesIsPresent)
+                .filter(isNotExpired)
+                .isPresent();
+    }
+
+    private final Predicate<DecodedJWT> usernameIsPresent = decodedJWT -> Optional.ofNullable(decodedJWT)
+            .map(Payload::getSubject)
+            .isPresent();
+
+    private final Predicate<DecodedJWT> rolesIsPresent = decodedJWT -> Optional.ofNullable(decodedJWT)
+            .map(jwt -> jwt.getClaim("roles"))
+            .isPresent();
+
+    private final Predicate<DecodedJWT> isNotExpired = decodedJWT -> Optional.ofNullable(decodedJWT)
+            .map(Payload::getExpiresAt)
+            .filter(expirationDate -> new Date(System.currentTimeMillis()).before(expirationDate))
+            .isPresent();
+
+    public String getUsername(String authenticationToken) {
+        return Optional.ofNullable(getDecodedJWT(authenticationToken))
+                .map(Payload::getSubject)
+                .orElseThrow(() -> new ValidationException("it was not possible to obtain username"));
     }
 
     public Set<SimpleGrantedAuthority> getAuthorities(String authenticationToken) {
-        var decodedJWT = getDecodedJWT(authenticationToken);
-
-        if (Objects.nonNull(decodedJWT)) {
-            var roles = decodedJWT.getClaim("roles").asArray(String.class);
-
-            return Stream.of(roles).map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
-        }
-
-        return null;
+        return Optional.ofNullable(getDecodedJWT(authenticationToken))
+                .map(decodedJWT -> decodedJWT.getClaim("roles")
+                        .asList(String.class))
+                .map(listOfRoles -> listOfRoles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toSet()))
+                .orElseThrow(() -> new ValidationException("it was not possible to obtain authorities"));
     }
 }
