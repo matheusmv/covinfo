df40ba333397213010ba9a64b550eacb992aeddc
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-27 11:18:44 -0300

    improving implementation of user authentication service

--- backend/security/src/main/java/br/edu/ifce/security/UserAuthenticationServiceImpl.java
+++ backend/security/src/main/java/br/edu/ifce/security/UserAuthenticationServiceImpl.java
@@ -2,44 +2,46 @@ package br.edu.ifce.security;
 
 import br.edu.ifce.security.jwt.JWTUtil;
 import br.edu.ifce.security.user.UserSecurityService;
+import br.edu.ifce.usecase.exceptions.AuthorizationException;
 import br.edu.ifce.usecase.ports.driven.UserAuthenticationService;
-import br.edu.ifce.usecase.ports.driven.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserAuthenticationServiceImpl implements UserAuthenticationService {
 
-    @Autowired
-    private JWTUtil jwtUtil;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
+    private final JWTUtil jwtUtil;
 
     @Override
     public AuthenticatedUser getAuthenticatedUser() {
-        try {
-            var authentication = SecurityContextHolder.getContext().getAuthentication();
-
-            var email = authentication.getPrincipal().toString();
-            var isAuthenticated = authentication.isAuthenticated();
-            var roles = authentication.getAuthorities().stream()
-                    .map(Objects::toString)
-                    .collect(Collectors.toSet());
-
-            return new AuthenticatedUser(email, isAuthenticated, roles);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(toAuthenticatedUser)
+                .orElseThrow(() -> new AuthorizationException("unauthenticated"));
     }
+
+    private final Function<Authentication, AuthenticatedUser> toAuthenticatedUser = authenticatedUser -> {
+        var email = authenticatedUser.getPrincipal().toString();
+        var isAuthenticated = authenticatedUser.isAuthenticated();
+        var roles = authenticatedUser.getAuthorities().stream()
+                .map(Objects::toString)
+                .collect(Collectors.toSet());
+
+        return new AuthenticatedUser(email, isAuthenticated, roles);
+    };
 
     @Override
     public String refreshAuthToken(String email) {
