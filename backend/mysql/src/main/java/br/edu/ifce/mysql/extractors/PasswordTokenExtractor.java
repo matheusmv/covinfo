8502da8f57f9bb9e32a89f33a9c93666232df54b
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-28 15:58:05 -0300

    add PasswordToken jdbcTemplate Repository

--- backend/mysql/src/main/java/br/edu/ifce/mysql/extractors/PasswordTokenExtractor.java
+++ backend/mysql/src/main/java/br/edu/ifce/mysql/extractors/PasswordTokenExtractor.java
@@ -3,19 +3,35 @@ package br.edu.ifce.mysql.extractors;
 import br.edu.ifce.domain.PasswordToken;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
 
+@Component
 public class PasswordTokenExtractor implements ResultSetExtractor<Set<PasswordToken>> {
 
     @Override
     public Set<PasswordToken> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
-        return null;
+        Set<PasswordToken> passwordTokens = new HashSet<>();
+
+        while (resultSet.next()) {
+            passwordTokens.add(mapPasswordToken(resultSet));
+        }
+
+        return passwordTokens;
     }
 
+    // TODO: remove the user attribute from the PasswordToken entity
     private PasswordToken mapPasswordToken(ResultSet resultSet) throws SQLException {
-        return new PasswordToken();
+        return new PasswordToken(
+                resultSet.getLong("id"),
+                resultSet.getString("token"),
+                resultSet.getTimestamp("created_at").toLocalDateTime(),
+                resultSet.getTimestamp("expires_at").toLocalDateTime(),
+                null
+        );
     }
 }
--- backend/mysql/src/main/java/br/edu/ifce/mysql/jdbc/JdbcPasswordTokenRepository.java
+++ backend/mysql/src/main/java/br/edu/ifce/mysql/jdbc/JdbcPasswordTokenRepository.java
@@ -1,4 +1,106 @@
 package br.edu.ifce.mysql.jdbc;
 
-public class JdbcPasswordTokenRepository {
+import br.edu.ifce.domain.PasswordToken;
+import br.edu.ifce.mysql.Page;
+import br.edu.ifce.mysql.PageRequest;
+import br.edu.ifce.mysql.extractors.PasswordTokenExtractor;
+import br.edu.ifce.mysql.repository.PasswordTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class JdbcPasswordTokenRepository implements PasswordTokenRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final PasswordTokenExtractor passwordTokenExtractor;
+
+    private static final String BASE_QUERY = "SELECT " +
+            "id, token, created_at, expires_at " +
+            "FROM password_token";
+
+    @Override
+    public PasswordToken create(PasswordToken passwordToken) {
+        var insertStatement = "INSERT INTO password_token VALUES (? ? ? ?)";
+
+        jdbcTemplate.update(insertStatement,
+                passwordToken.getId(),
+                passwordToken.getToken(),
+                passwordToken.getCreatedAt(),
+                passwordToken.getExpiresAt()
+        );
+
+        return find(passwordToken.getId()).orElse(null);
+    }
+
+    @Override
+    public PasswordToken update(PasswordToken passwordToken) {
+        var updateStatement = "UPDATE password_token SET " +
+                "id = ?, " +
+                "token = ?, " +
+                "created_at = ?, " +
+                "expires_at = ? " +
+                "WHERE password_token.id = ?";
+
+        jdbcTemplate.update(updateStatement,
+                passwordToken.getId(),
+                passwordToken.getToken(),
+                passwordToken.getCreatedAt(),
+                passwordToken.getExpiresAt()
+        );
+
+        return find(passwordToken.getId()).orElse(null);
+    }
+
+    @Override
+    public Optional<PasswordToken> find(Long id) {
+        var selectStatement = BASE_QUERY + " WHERE password_token.id = ?";
+
+        return Objects.requireNonNull(jdbcTemplate.query(selectStatement, passwordTokenExtractor, id)).stream().findFirst();
+    }
+
+    @Override
+    // TODO: improve paging implementation
+    public Page<PasswordToken> find(PageRequest page) {
+        var selectStatement = BASE_QUERY + " LIMIT ?" + " OFFSET ?";
+        var queryArguments = Arrays.asList(page.getSize(), page.offset()).toArray();
+
+        var setOfPasswordTokens = jdbcTemplate.query(selectStatement, passwordTokenExtractor, queryArguments);
+
+        return new Page<>(
+                Stream.ofNullable(setOfPasswordTokens).flatMap(Collection::stream).collect(Collectors.toList()),
+                page.getPage(),
+                page.getSize(),
+                executeCountQuery()
+        );
+    }
+
+    private Integer executeCountQuery() {
+        var countStatement = "SELECT count(*) AS total FROM password_token";
+
+        return jdbcTemplate.queryForObject(countStatement, Integer.class);
+    }
+
+    @Override
+    public void delete(PasswordToken passwordToken) {
+        var deleteStatement = "DELETE FROM password_token WHERE password_token.id = ?";
+
+        jdbcTemplate.update(deleteStatement, passwordToken.getId());
+    }
+
+    @Override
+    public void delete(Long id) {
+        var deleteStatement = "DELETE FROM password_token WHERE password_token.id = ?";
+
+        jdbcTemplate.update(deleteStatement, id);
+    }
 }
